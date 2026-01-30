8d5afe16611a15eb1f4b848616184ff95852a7ad
Haifaa Tafich <haifaata9186@gmail.com>
2026-01-30 14:32:26 +0100

    Add library and unit tests

---
+++ MyLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using MyLibrary;
+
+namespace MyLibrary.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        // Arrange
+        var calc = new Calculator();
+
+        // Act
+        int result = calc.Add(2, 3);
+
+        // Assert
+        Assert.Equal(5, result);
+    }
+}
